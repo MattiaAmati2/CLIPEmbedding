46d83f5e73f67256135e3d68805ed7fc97bd3d94
Mattia <mattia.amati04@gmail.com>
2026-03-18 16:50:13 +0100

    fix: made the required changes to the embedding script

--- embedding_script.py
+++ embedding_script.py
@@ -26,11 +26,10 @@ def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--dataset", type=str, required=True)
     parser.add_argument("--model", type=str, required=True, choices = ["openai/clip-vit-base-patch16",
                                                                                    "openai/clip-vit-base-patch32"])
-    parser.add_argument("--dataset", type=str, required=True)
-
-    args : argparse.Namespace = parser.parse_args()
+    args = parser.parse_args()
 
     image_label, text_label = find_supervised_keys(args.dataset)
 
@@ -41,44 +40,50 @@ def main():
     label_translator = datasets.get_dataset_config_info(args.dataset).features[text_label]
 
     def image_preprocess(raw_batch):
-        if isinstance(label_translator, datasets.ClassLabel):
-            raw_batch[text_label] = label_translator.int2str(raw_batch[text_label])
+        processed_images = processor(images = raw_batch[image_label], return_tensors="pt", padding=True)
+        processed_images[text_label] = raw_batch[text_label]
 
-        raw_batch[text_label] = ["A photo of a " + item for item in raw_batch[text_label]]
-
-        return processor(text=raw_batch[text_label], images=raw_batch[image_label],
-                         return_tensors="pt", padding=True)
-    @torch.no_grad()
-    def compute_embeddings(image_batch, text_batch, attention_mask):
-        image_batch = image_batch.to(device)
-        text_batch = text_batch.to(device)
-        attention_mask = attention_mask.to(device)
-
-        return  model.get_image_features(image_batch).cpu(), model.get_text_features(text_batch, attention_mask).cpu()
+        return processed_images
 
     splits = datasets.get_dataset_split_names(args.dataset)
     dataset_splits = {}
     for split in splits:
-        dataset_splits[split] = load_dataset(args.dataset, split=split).with_transform(image_preprocess)
+        dataset_splits[split] = load_dataset(args.dataset, split=split)
 
+    unique_labels = sorted(list(set(dataset_splits["train"][text_label])))
+
+    if isinstance(label_translator, datasets.ClassLabel):
+        unique_labels = label_translator.int2str(unique_labels)
+
+    unique_labels = [f"A photo of a {label}" for label in unique_labels]
+    processed_labels = processor(text = unique_labels, return_tensors="pt", padding = True).to(device)
+    text_embeddings = model.get_text_features(**processed_labels).cpu()
+
+    print("text_embeddings.shape", text_embeddings.shape)
+
+    for split in splits:
+        dataset_splits[split] = dataset_splits[split].with_transform(image_preprocess)
 
     for key in dataset_splits:
-        data_loader = torch.utils.data.DataLoader(dataset_splits[key], batch_size=512, shuffle=False, num_workers=8)
+        data_loader = torch.utils.data.DataLoader(dataset_splits[key], batch_size=256, shuffle=False, num_workers=16)
+
         image_embeddings = []
-        text_embeddings = []
+        text_labels = []
         for batch in tqdm(data_loader, desc = "Computing split: " + key):
-            image_result, text_result = compute_embeddings(batch["pixel_values"],
-                                                           batch["input_ids"],
-                                                           batch["attention_mask"])
-            image_embeddings.append(image_result)
-            text_embeddings.append(text_result)
+            with (torch.no_grad()):
+                image_batch = batch["pixel_values"].to(device)
+                image_embeddings.append(model.get_image_features(image_batch).cpu())
+                text_labels.extend(batch[text_label])
 
-        embeddings_dict = {
-            "images": torch.cat(image_embeddings),
-            "texts": torch.cat(text_embeddings)
+        results_dict = {
+            "image_embeddings": torch.cat(image_embeddings),
+            "text_embeddings" : torch.nn.functional.normalize(text_embeddings, p=2, dim=1),
+            "labels": text_labels,
+            "class_names" : unique_labels
         }
-        torch.save(embeddings_dict, f"{args.model.replace('/', '-')}__{args.dataset.replace('/', '-')}__{key}__embeddings.pt")
 
+        results_dict["image_embeddings"] = torch.nn.functional.normalize(results_dict["image_embeddings"], p=2, dim=1)
+        torch.save(results_dict, f"{args.model.replace('/', '-')}__{args.dataset.replace('/', '-')}__{key}__embeddings.pt")
 
 if __name__ == "__main__":
     main()
